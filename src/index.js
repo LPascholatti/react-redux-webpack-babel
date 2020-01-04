1667a94482cb6b5a211b6e72e6b7ae695b962b99
LPascholatti <lucaspascholatti@gmail.com>
2020-01-04 18:18:13 +0100

    Removed one unnecessary import of bootstrap in index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
