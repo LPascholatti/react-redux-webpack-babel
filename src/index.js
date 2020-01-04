02ce77ebfabe53f329dd25f2831fb2359b991fa6
LPascholatti <lucaspascholatti@gmail.com>
2020-01-04 18:14:15 +0100

    Installed new dependencies

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
